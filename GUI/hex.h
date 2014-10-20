3d06a8a60508d96ed3ac1d9fb9ca619fce03ac0b
Gib Bogle <g.bogle@auckland.ac.nz>
2014-10-20 13:52:15 +1300

    G: user: Gib Bogle <g.bogle@auckland.ac.nz> G: branch 'default' G: added GUI/hex.h G: added GUI/result_set.h

---
+++ GUI/hex.h
@@ -0,0 +1,14 @@
+#ifndef HEX_H
+#define HEX_H
+
+struct point3d_str {
+    double x[3];
+};
+typedef point3d_str POINT3D;
+
+struct hexahedron {
+    POINT3D vertex[8];
+};
+typedef hexahedron HEXAHEDRON;
+
+#endif // HEX_H
